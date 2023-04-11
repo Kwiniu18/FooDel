e56248c10e1e2ed0397737cd903c35581a5f5ddf
Kwiniu18 <dom.kwintal@interia.pl>
2023-04-11 15:39:33 +0000

    simple-content

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import "./App.css";
 import NavbarTop from "./Components/NavbarTop";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Styles/index.scss";
+import Content from "./Components/Content";
 
 function App() {
-  return <NavbarTop />;
+  return (
+    <>
+      <NavbarTop />
+      <Content />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Components/Content.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Content() {
+  return (
+    <div className="content-section">
+      <div className="content-title">Zacznij zamawiać</div>
+      <div className="content-btn">
+        <button className="start-btn">Start</button>
+      </div>
+    </div>
+  );
+}
+
+export default Content;
