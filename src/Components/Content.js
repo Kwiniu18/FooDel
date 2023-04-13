ae92de8bd80c9b050bc6dd53dcd73a59865f5e08
Kwiniu18 <dom.kwintal@interia.pl>
2023-04-13 14:57:16 +0000

    simple products cards

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,11 +1,38 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function Content() {
+  const cards = [
+    {
+      image_url: "",
+      card_name: "produkt",
+    },
+    {
+      card_name: "produkt2",
+    },
+    {
+      card_name: "produkt3",
+    },
+  ];
+
   return (
     <div className="content-section">
       <div className="content-title">Zacznij zamawiać</div>
       <div className="content-btn">
-        <button className="start-btn">Start</button>
+        <Button variant="success" className="start-btn">
+          Start
+        </Button>
+      </div>
+      <div className="content-cards">
+        <Row>
+          {cards.map((e) => (
+            <Col>
+              <div className="card">{e.card_name}</div>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
--- src/Components/NavbarTop.js
+++ src/Components/NavbarTop.js
@@ -12,8 +12,8 @@ function NavbarTop() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">Start</Nav.Link>
+            <Nav.Link href="#link">Jak działamy?</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
